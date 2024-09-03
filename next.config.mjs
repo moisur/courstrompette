6e5fd89d517fb38d8aea54b00fa18bc279b2c94f
moisur <contact@lecoledes1.com>
2024-09-03 19:15:50 +0200

    FINITO

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/courstrompetteparis',
+          destination: 'https://courstrompette.vercel.app',
+        },
+        {
+          source: '/courstrompetteparis/:path*',
+          destination: 'https://courstrompette.vercel.app/:path*',
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
