a680867b71f31209ccb4a55d37f7141cc8632d01
moisur <contact@lecoledes1.com>
2024-10-01 19:14:50 +0200

    trustpilot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,10 @@ export default function Home() {
   return (
     <main className="relative min-h-screen">
       <meta name="google-site-verification" content="jBElafHqU3eAux7x5QbUblWVpm3kVEjzME6ZKlXzglU" />
+      <meta
+          name="trustpilot-one-time-domain-verification-id"
+          content="54137254-11d5-43e0-8675-77ebb324d3b0"
+        />
       <div className="z-0">
         <Cours />
       </div>
