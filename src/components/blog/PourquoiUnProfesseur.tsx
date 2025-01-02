d9ce806a16969ea49ee5e9e65011bd91ff056ba1
moisur <contact@lecoledes1.com>
2025-01-02 14:02:14 +0100

    nouveau blog

--- src/components/blog/PourquoiUnProfesseur.tsx
+++ src/components/blog/PourquoiUnProfesseur.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
+
+
 'use client';
 
-import Image from 'next/image';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import AccessoiresTrompette from './AccessoireRecommandes';
 import RelatedArticles from './RelatedArticles';
@@ -26,20 +28,20 @@ export default function PourquoiUnProfesseur() {
 
       {/* Accordion Section */}
       <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="mauvaisesHabitudes">
-            <AccordionTrigger className="text-xl font-semibold text-primary">Éviter les mauvaises habitudes, un gain de temps précieux</AccordionTrigger>
-            <AccordionContent>
-              <p>Apprendre seul, c'est s'exposer au risque de développer de mauvaises habitudes, parfois invisibles au début, mais qui peuvent devenir de véritables freins à votre progression. "Sans personne pour vous dire ce que vous faites de mal, […] vous pourriez finir par faire quelque chose de <i><b>très mal</b></i> sans jamais vous en rendre compte", témoigne un trompettiste. Un professeur, avec son œil expert, repérera et corrigera ces erreurs dès le départ, vous évitant ainsi des mois, voire des années, de frustration. "J'avais l'habitude de parler avec ma gorge jusqu'à ce que mon professeur de groupe l'attrape", confirme un autre musicien. Imaginez le temps perdu à ancrer une mauvaise technique !</p>
-            </AccordionContent>
-          </AccordionItem>
+        <AccordionItem value="mauvaisesHabitudes">
+          <AccordionTrigger className="text-xl font-semibold text-primary">Éviter les mauvaises habitudes, un gain de temps précieux</AccordionTrigger>
+          <AccordionContent>
+            <p>Apprendre seul, c'est s'exposer au risque de développer de mauvaises habitudes, parfois invisibles au début, mais qui peuvent devenir de véritables freins à votre progression. "Sans personne pour vous dire ce que vous faites de mal, […] vous pourriez finir par faire quelque chose de <i><b>très mal</b></i> sans jamais vous en rendre compte", témoigne un trompettiste. Un professeur, avec son œil expert, repérera et corrigera ces erreurs dès le départ, vous évitant ainsi des mois, voire des années, de frustration. "J'avais l'habitude de parler avec ma gorge jusqu'à ce que mon professeur de groupe l'attrape", confirme un autre musicien. Imaginez le temps perdu à ancrer une mauvaise technique !</p>
+          </AccordionContent>
+        </AccordionItem>
 
-          <AccordionItem value="accompagnement">
-            <AccordionTrigger className="text-xl font-semibold text-primary">Un accompagnement personnalisé, la clé du succès</AccordionTrigger>
-            <AccordionContent>
+        <AccordionItem value="accompagnement">
+          <AccordionTrigger className="text-xl font-semibold text-primary">Un accompagnement personnalisé, la clé du succès</AccordionTrigger>
+          <AccordionContent>
             <p>Chaque individu est différent. La forme de votre bouche, l'épaisseur de vos lèvres, votre morphologie… tous ces éléments influencent votre façon de jouer. "La façon dont vous jouez sera différente de la façon dont quelqu'un d'autre joue en raison de l'état de votre corps", explique un musicien expérimenté. Un professeur adaptera sa pédagogie à vos spécificités, vous offrant un accompagnement <i><b>personnalisé</b></i> et <i><b>optimisé</b></i> pour votre progression. Les tutoriels en ligne, aussi complets soient-ils, ne peuvent pas offrir cette individualisation.
             </p>
-            </AccordionContent>
-          </AccordionItem>
+          </AccordionContent>
+        </AccordionItem>
 
         <AccordionItem value="motivation">
           <AccordionTrigger className="text-xl font-semibold text-primary">Motivation et inspiration, le carburant de votre apprentissage</AccordionTrigger>
@@ -51,7 +53,7 @@ export default function PourquoiUnProfesseur() {
         <AccordionItem value="ressources">
           <AccordionTrigger className="text-xl font-semibold text-primary">Des ressources en ligne, un complément, pas un substitut</AccordionTrigger>
           <AccordionContent>
-              <p>Certes, il existe d'excellentes ressources en ligne, comme les méthodes "Du mystère à la maîtrise" ou "Power Player Trompette". Elles peuvent être un complément <i><b>utile</b></i> à l'enseignement d'un professeur, mais ne peuvent en aucun cas le remplacer. "Même si les ressources en ligne peuvent être utiles dans une certaine mesure, <i><b>OBTENEZ UN PROFESSEUR</b></i>".</p>
+            <p>Certes, il existe d'excellentes ressources en ligne, comme les méthodes "Du mystère à la maîtrise" ou "Power Player Trompette". Elles peuvent être un complément <i><b>utile</b></i> à l'enseignement d'un professeur, mais ne peuvent en aucun cas le remplacer. "Même si les ressources en ligne peuvent être utiles dans une certaine mesure, <i><b>OBTENEZ UN PROFESSEUR</b></i>".</p>
           </AccordionContent>
         </AccordionItem>
       </Accordion>
@@ -59,12 +61,12 @@ export default function PourquoiUnProfesseur() {
 
       {/* Call to Action */}
       <div className="text-center">
-      <p className="text-xl text-gray-700 mb-6">
-      Si vous êtes <i><b>sérieux</b></i> dans votre désir d’apprendre la trompette, investir dans un professeur est la <i><b>meilleure décision</b></i> que vous puissiez prendre. C’est un investissement en vous-même, en votre passion, en votre avenir musical. Alors, n’hésitez plus, trouvez le professeur qui vous correspond et lancez-vous dans cette merveilleuse aventure !
-      </p>        <a 
-          href="/#booking" 
+        <p className="text-xl text-gray-700 mb-6">
+          Si vous êtes <i><b>sérieux</b></i> dans votre désir d’apprendre la trompette, investir dans un professeur est la <i><b>meilleure décision</b></i> que vous puissiez prendre. C’est un investissement en vous-même, en votre passion, en votre avenir musical. Alors, n’hésitez plus, trouvez le professeur qui vous correspond et lancez-vous dans cette merveilleuse aventure !
+        </p>        <a
+          href="/#booking"
           className="w-full bg-orange-500 text-white font-semibold py-2 px-4 rounded hover:bg-orange-600 transition duration-300"
-          >
+        >
           Réserver mon cours gratuit
         </a>
       </div>
