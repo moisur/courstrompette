6b107bf90a59ea8fa18acfe2c67ba422ec1de974
moisur <contact@lecoledes1.com>
2026-02-09 13:49:47 +0100

    security: hide Powered-By header

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
+  poweredByHeader: false,
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
